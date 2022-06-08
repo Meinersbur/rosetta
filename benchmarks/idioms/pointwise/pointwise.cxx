8202e8a7b2317bae055291ada15b7b5cb3a850a2
Michael Kruse <xcellent-test-suite@meinersbur.de>
2022-06-08 16:48:40 -0500

    multarray compile fix

--- benchmarks/idioms/pointwise/pointwise.cxx
+++ benchmarks/idioms/pointwise/pointwise.cxx
@@ -2,13 +2,13 @@
 
 
 
-static void kernel(int n, double *A) {
+static void kernel(int n, real *A) {
     for (int i = 0; i < n; i += 1)
         A[i] += 42;
 }
 
 void run(State& state, int n) {
-    auto A  = state.fakedata_array<double>(n,/*verify*/true);   
+    auto A  = state.fakedata_array<real>(n,/*verify*/true);   
 
     for (auto &&_ : state) 
         kernel(n, A.data());    
