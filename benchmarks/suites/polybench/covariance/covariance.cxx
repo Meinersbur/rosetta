d2acc0936804a71731cf73943660075f277a760f
Michael Kruse <xcellent-test-suite@meinersbur.de>
2022-06-10 15:33:11 -0500

    Add polybench covariance

---
+++ benchmarks/suites/polybench/covariance/covariance.cxx
@@ -0,0 +1,48 @@
+#include "rosetta.h"
+
+
+static
+void kernel(int m, int n,
+			real float_n,
+             multarray<real,2> data,
+                multarray<real,2> cov,
+                real *mean){
+#pragma scop
+  for (int j = 0; j < m; j++)    {
+      mean[j] = 0.0;
+      for (int i = 0; i < n; i++)
+        mean[j] += data[i][j];
+      mean[j] /= float_n;
+    }
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      data[i][j] -= mean[j];
+
+  for (int i = 0; i < m; i++)
+    for (int j = i; j < m; j++)      {
+        cov[i][j] = 0.0;
+        for (int k = 0; k < n; k++)
+	  cov[i][j] += data[k][i] * data[k][j];
+        cov[i][j] /= (float_n - 1.0);
+        cov[j][i] = cov[i][j];
+      }
+#pragma endscop
+}
+
+
+void run(State& state, int pbsize) {
+    // n is 8%-25% larger than m
+    size_t n = pbsize;
+    size_t m = pbsize - pbsize/8;
+
+    real float_n = n;
+    auto data = state.allocate_array<real>({m,n}, /*fakedata*/true, /*verify*/false);
+    auto cov = state.allocate_array<real>({ m,m },/*fakedata*/false,/*verify*/true);
+    auto mean = state.allocate_array<real>({ m },/*fakedata*/false,/*verify*/true );
+
+  
+
+    for (auto &&_ : state) 
+        kernel(m, n, float_n,  data, cov, mean);
+}
