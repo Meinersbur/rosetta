c966c513febed7b2514de2c62b630f9c584fa6a4
Michael Kruse <xcellent-test-suite@meinersbur.de>
2022-07-25 11:45:21 -0500

    clang-format polybenhc

--- benchmarks/suites/polybench/gemm/gemm.cxx
+++ benchmarks/suites/polybench/gemm/gemm.cxx
@@ -1,36 +1,34 @@
 #include "rosetta.h"
 
 
-static
-void kernel(int ni, int nj,int nk,
-real alpha,
-real beta,
- multarray<real,2> C,    multarray<real,2> A,  multarray<real,2> B ){
+static void kernel(int ni, int nj, int nk,
+                   real alpha,
+                   real beta,
+                   multarray<real, 2> C, multarray<real, 2> A, multarray<real, 2> B) {
 #pragma scop
   for (int i = 0; i < ni; i++) {
     for (int j = 0; j < nj; j++)
-	C[i][j] *= beta;
+      C[i][j] *= beta;
     for (int k = 0; k < nk; k++) {
-       for (int j = 0; j < nj; j++)
-	  C[i][j] += alpha * A[i][k] * B[k][j];
+      for (int j = 0; j < nj; j++)
+        C[i][j] += alpha * A[i][k] * B[k][j];
     }
   }
 #pragma endscop
-
 }
 
 
-void run(State& state, int pbsize) {
-    size_t ni = pbsize - pbsize/4;
-    size_t nj = pbsize - pbsize/8;
-    size_t nk = pbsize;
+void run(State &state, int pbsize) {
+  size_t ni = pbsize - pbsize / 4;
+  size_t nj = pbsize - pbsize / 8;
+  size_t nk = pbsize;
 
   real alpha = 1.5;
   real beta = 1.2;
-    auto C = state.allocate_array<double>({ni,nj},  /*fakedata*/true, /*verify*/true );
-    auto A = state.allocate_array<double>({ ni,nk },  /*fakedata*/true,/*verify*/false);
-    auto B = state.allocate_array<double>({ nk,nj },  /*fakedata*/true,/*verify*/false);
+  auto C = state.allocate_array<double>({ni, nj}, /*fakedata*/ true, /*verify*/ true);
+  auto A = state.allocate_array<double>({ni, nk}, /*fakedata*/ true, /*verify*/ false);
+  auto B = state.allocate_array<double>({nk, nj}, /*fakedata*/ true, /*verify*/ false);
 
-    for (auto &&_ : state) 
-        kernel(ni,nj,nk, alpha, beta, C,A,B);
+  for (auto &&_ : state)
+    kernel(ni, nj, nk, alpha, beta, C, A, B);
 }
--- benchmarks/suites/polybench/symm/symm.cxx
+++ benchmarks/suites/polybench/symm/symm.cxx
@@ -1,45 +1,38 @@
 #include "rosetta.h"
 
 
-static
-void kernel(int m, int n,
-real alpha, real beta,
- multarray<real,2> C,
- multarray<real,2> A,
- multarray<real,2> B  ){
+static void kernel(int m, int n,
+                   real alpha, real beta,
+                   multarray<real, 2> C,
+                   multarray<real, 2> A,
+                   multarray<real, 2> B) {
 #pragma scop
-   for (int i = 0; i < m; i++)
-      for (int j = 0; j < n; j++ )      {
-       real  temp2 = 0;
-        for (int k = 0; k < i; k++) {
-           C[k][j] += alpha*B[i][j] * A[i][k];
-           temp2 += B[k][j] * A[i][k];
-        }
-        C[i][j] = beta * C[i][j] + alpha*B[i][j] * A[i][i] + alpha * temp2;
-     }
+  for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++) {
+      real temp2 = 0;
+      for (int k = 0; k < i; k++) {
+        C[k][j] += alpha * B[i][j] * A[i][k];
+        temp2 += B[k][j] * A[i][k];
+      }
+      C[i][j] = beta * C[i][j] + alpha * B[i][j] * A[i][i] + alpha * temp2;
+    }
 #pragma endscop
 }
 
 
 
+void run(State &state, int pbsize) {
+  size_t n = pbsize;
+  size_t m = pbsize - pbsize / 8;
 
-void run(State& state, int pbsize) {
-    size_t n = pbsize;
-    size_t m = pbsize - pbsize/8;
 
-
-    real alpha = 1.5;
+  real alpha = 1.5;
   real beta = 1.2;
-    auto C = state.allocate_array<double>({m,n},  /*fakedata*/false, /*verify*/true );
-    auto A = state.allocate_array<double>({ m,m },  /*fakedata*/true,/*verify*/false);
-    auto B = state.allocate_array<double>({ m,n },  /*fakedata*/true,/*verify*/false);
+  auto C = state.allocate_array<double>({m, n}, /*fakedata*/ false, /*verify*/ true);
+  auto A = state.allocate_array<double>({m, m}, /*fakedata*/ true, /*verify*/ false);
+  auto B = state.allocate_array<double>({m, n}, /*fakedata*/ true, /*verify*/ false);
 
 
-    for (auto &&_ : state)
-        kernel(m,n,  alpha , beta,C,A,B);
+  for (auto &&_ : state)
+    kernel(m, n, alpha, beta, C, A, B);
 }
-
-
-
-
-
