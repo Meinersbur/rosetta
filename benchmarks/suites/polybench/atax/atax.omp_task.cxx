08a3b70cd2dacf65da0fb96b912e692705023e8c
Michael Kruse <xcellent-test-suite@meinersbur.de>
2022-08-25 17:16:22 -0500

    Fix verify

--- benchmarks/suites/polybench/atax/atax.omp_task.cxx
+++ benchmarks/suites/polybench/atax/atax.omp_task.cxx
@@ -3,23 +3,24 @@
 
 
 static void kernel(int m, int n, multarray<real, 2> A, real *x, real *y, real *tmp) {
-  #pragma omp parallel
-  {
+#pragma omp parallel master
+    {
+
 #pragma omp taskloop
-  for (int i = 0; i < n; i++)
-    y[i] = 0;
-#pragma omp taskloop
-  for (int i = 0; i < m; i++) {
-    tmp[i] = 0;
-    for (int j = 0; j < n; j++)
-      tmp[i] = tmp[i] + A[i][j] * x[j];
-  }
+        for (int i = 0; i < n; i++)
+            y[i] = 0;
+#pragma omp  taskloop
+        for (int i = 0; i < m; i++){
+            tmp[i] = 0;
+            for (int j = 0; j < n; j++)
+                tmp[i] += A[i][j] * x[j];
+        }
 #pragma omp taskwait
 #pragma omp taskloop
-   for (int j = 0; j < n; j++)
-        for (int i = 0; i < m; i++)
-      y[j] = y[j] + A[i][j] * tmp[i];
-  }
+        for (int j = 0; j < n; j++)
+            for (int i = 0; i < m; i++)
+                y[j] += A[i][j] * tmp[i];
+    }
 }
 
 
@@ -28,11 +29,11 @@ void run(State &state, int pbsize) {
   pbsize_t n = pbsize;
   pbsize_t m = pbsize - pbsize / 10;
 
-  auto A = state.allocate_array<double>({n, m}, /*fakedata*/ true, /*verify*/ false);
-  auto x = state.allocate_array<double>({n}, /*fakedata*/ false, /*verify*/ false);
-  auto y = state.allocate_array<double>({n}, /*fakedata*/ false, /*verify*/ true);
-  auto tmp = state.allocate_array<double>({n}, /*fakedata*/ false, /*verify*/ false);
+  auto A = state.allocate_array<double>({ m,n}, /*fakedata*/ true, /*verify*/ false, "A");
+  auto x = state.allocate_array<double>({n}, /*fakedata*/ true, /*verify*/ false, "x");
+  auto y = state.allocate_array<double>({n}, /*fakedata*/ false, /*verify*/ true, "y");
+  auto tmp = state.allocate_array<double>({m}, /*fakedata*/ false, /*verify*/ false, "tmp");
 
   for (auto &&_ : state)
-    kernel(n, m, A, x, y, tmp);
+    kernel(m,n,  A, x, y, tmp);
 }
