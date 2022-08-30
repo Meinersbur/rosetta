81c71048e5813cdf8ef487dbaaa8db82f086294a
Michael Kruse <xcellent-test-suite@meinersbur.de>
2022-08-29 19:17:51 -0500

    Implement --ccs RandomTreeTuner strategy

--- rosetta/lib/rosetta.py
+++ rosetta/lib/rosetta.py
@@ -7,7 +7,7 @@
 # TODO: Clenaup file separation
 from cmdtool import *
 import invoke
-from runner import runner_main,register_benchmark,load_register_file,rosetta_config,runner_main_verify,runner_main_run
+from runner import runner_main,register_benchmark,load_register_file,rosetta_config,runner_main_verify,runner_main_run,runner_main_probe
 import runner
 from orderedset import OrderedSet
 from runner import Benchmark
