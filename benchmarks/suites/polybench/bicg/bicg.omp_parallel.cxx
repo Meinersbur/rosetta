c18cb0f5b70b9dfe9314757350b0542be0482911
Michael Kruse <xcellent-test-suite@meinersbur.de>
2022-10-22 00:03:00 -0500

    Fix parallel bicg

--- benchmarks/suites/polybench/bicg/bicg.omp_parallel.cxx
+++ benchmarks/suites/polybench/bicg/bicg.omp_parallel.cxx
@@ -5,17 +5,18 @@
 static void kernel(int m, int n, multarray<real, 2> A, real s[], real q[], real p[], real r[]) {
   #pragma omp parallel
   {
-    #pragma omp for schedule(static)
-  for (int i = 0; i < m; i++)
-    s[i] = 0;
-        #pragma omp for schedule(static)
+       #pragma omp for schedule(static) nowait
   for (int i = 0; i < n; i++) {
     q[i] = 0;
-    for (int j = 0; j < m; j++) {
-      s[j] = s[j] + r[i] * A[i][j];
-      q[i] = q[i] + A[i][j] * p[j];
-    }
+    for (int j = 0; j < m; j++)
+      q[i] += A[i][j] * p[j];
   }
+      #pragma omp for  schedule(static) nowait
+            for (int j = 0; j < m; j++) {
+                  s[j] = 0;
+      for (int i = 0; i < n; i++)
+        s[j] += r[i] * A[i][j];
+            }
   }
 }
 
@@ -24,11 +25,12 @@ void run(State &state, int pbsize) {
   size_t m = pbsize - 19 * pbsize / 21; // 1900
   size_t n = pbsize;                    // 2100
 
-  auto A = state.allocate_array<double>({n, m}, /*fakedata*/ true, /*verify*/ false);
-  auto s = state.allocate_array<double>({m}, /*fakedata*/ false, /*verify*/ true);
-  auto q = state.allocate_array<double>({n}, /*fakedata*/ false, /*verify*/ true);
-  auto p = state.allocate_array<double>({m}, /*fakedata*/ true, /*verify*/ false);
-  auto r = state.allocate_array<double>({n}, /*fakedata*/ true, /*verify*/ false);
+  auto A = state.allocate_array<double>({n, m}, /*fakedata*/ true, /*verify*/ false, "A");
+  auto s = state.allocate_array<double>({m}, /*fakedata*/ false, /*verify*/ true, "s");
+  auto q = state.allocate_array<double>({n}, /*fakedata*/ false, /*verify*/ true, "q");
+  auto p = state.allocate_array<double>({m}, /*fakedata*/ true, /*verify*/ false, "p");
+  auto r = state.allocate_array<double>({n}, /*fakedata*/ true, /*verify*/ false, "r");
+
 
 
   for (auto &&_ : state)
