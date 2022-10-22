be01d0e9a5415acee3655711bc307a63a13bfb60
Michael Kruse <xcellent-test-suite@meinersbur.de>
2022-10-22 00:03:00 -0500

    Fix parallel bicg

--- benchmarks/suites/polybench/bicg/bicg.cxx
+++ benchmarks/suites/polybench/bicg/bicg.cxx
@@ -11,8 +11,8 @@ static void kernel(int m, int n, multarray<real, 2> A, real s[], real q[], real 
   for (int i = 0; i < n; i++) {
     q[i] = 0;
     for (int j = 0; j < m; j++) {
-      s[j] = s[j] + r[i] * A[i][j];
-      q[i] = q[i] + A[i][j] * p[j];
+      s[j] += r[i] * A[i][j];
+      q[i] += A[i][j] * p[j];
     }
   }
 #pragma endscop
@@ -25,11 +25,11 @@ void run(State &state, int pbsize) {
 
 
 
-  auto A = state.allocate_array<double>({n, m}, /*fakedata*/ true, /*verify*/ false);
-  auto s = state.allocate_array<double>({m}, /*fakedata*/ false, /*verify*/ true);
-  auto q = state.allocate_array<double>({n}, /*fakedata*/ false, /*verify*/ true);
-  auto p = state.allocate_array<double>({m}, /*fakedata*/ true, /*verify*/ false);
-  auto r = state.allocate_array<double>({n}, /*fakedata*/ true, /*verify*/ false);
+  auto A = state.allocate_array<double>({n, m}, /*fakedata*/ true, /*verify*/ false, "A");
+  auto s = state.allocate_array<double>({m}, /*fakedata*/ false, /*verify*/ true, "s");
+  auto q = state.allocate_array<double>({n}, /*fakedata*/ false, /*verify*/ true, "q");
+  auto p = state.allocate_array<double>({m}, /*fakedata*/ true, /*verify*/ false, "p");
+  auto r = state.allocate_array<double>({n}, /*fakedata*/ true, /*verify*/ false, "r");
 
 
   for (auto &&_ : state)
