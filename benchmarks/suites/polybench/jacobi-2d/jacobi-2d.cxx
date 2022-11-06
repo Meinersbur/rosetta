83121d52a77270283b1cb3012bca01cbef28b044
Michael Kruse <xcellent-test-suite@meinersbur.de>
2022-11-06 06:42:10 -0600

    Add omp_parallel jacobi-2d

--- benchmarks/suites/polybench/jacobi-2d/jacobi-2d.cxx
+++ benchmarks/suites/polybench/jacobi-2d/jacobi-2d.cxx
@@ -4,24 +4,24 @@
 
 
 
-static void kernel(int tsteps, int n,
+static void kernel(pbsize_t tsteps, pbsize_t n,
                    multarray<real, 2> A, multarray<real, 2> B) {
 #pragma scop
-  for (int t = 0; t < tsteps; t++) {
-    for (int i = 1; i < n - 1; i++)
-      for (int j = 1; j < n - 1; j++)
-        B[i][j] = ((real)0.2) * (A[i][j] + A[i][j - 1] + A[i][1 + j] + A[1 + i][j] + A[i - 1][j]);
-    for (int i = 1; i < n - 1; i++)
-      for (int j = 1; j < n - 1; j++)
-        A[i][j] = ((real)0.2) * (B[i][j] + B[i][j - 1] + B[i][1 + j] + B[1 + i][j] + B[i - 1][j]);
+  for (idx_t t = 0; t < tsteps; t++) {
+    for (idx_t i = 1; i < n - 1; i++)
+      for (idx_t j = 1; j < n - 1; j++)
+        B[i][j] =  (A[i][j] + A[i][j - 1] + A[i][1 + j] + A[1 + i][j] + A[i - 1][j])/5;
+    for (idx_t i = 1; i < n - 1; i++)
+      for (idx_t j = 1; j < n - 1; j++)
+        A[i][j] =  (B[i][j] + B[i][j - 1] + B[i][1 + j] + B[1 + i][j] + B[i - 1][j])/5;
   }
 #pragma endscop
 }
 
 
-void run(State &state, int pbsize) {
-  size_t tsteps = 1; // 500
-  size_t n = pbsize; // 1300
+void run(State &state, pbsize_t pbsize) {
+    pbsize_t tsteps = 1; // 500
+    pbsize_t n = pbsize; // 1300
 
 
 
