b83cdc152457d39d6ec63145b256813b63201587
Michael Kruse <xcellent-test-suite@meinersbur.de>
2022-11-06 06:36:09 -0600

    Add omp_parallel heat-3d

---
+++ benchmarks/suites/polybench/heat-3d/heat-3d.omp_parallel.cxx
@@ -0,0 +1,47 @@
+// BUILD: add_benchmark(ppm=omp_parallel)
+
+#include "rosetta.h"
+
+
+
+static void kernel(pbsize_t tsteps, pbsize_t n,
+                   multarray<real, 3> A, multarray<real, 3> B) {
+#pragma omp parallel default(none) firstprivate(tsteps,n,A,B)
+                       {
+                           for (idx_t t = 1; t <= tsteps; t++) {
+
+#pragma omp for collapse(3) schedule(static)
+                               for (idx_t i = 1; i < n - 1; i++) 
+                                   for (idx_t j = 1; j < n - 1; j++) 
+                                       for (idx_t k = 1; k < n - 1; k++) {
+                                           B[i][j][k] = (real)(0.125) * (A[i + 1][j][k] - (real)(2.0) * A[i][j][k] + A[i - 1][j][k]) + (real)(0.125) * (A[i][j + 1][k] - (real)(2.0) * A[i][j][k] + A[i][j - 1][k]) + (real)(0.125) * (A[i][j][k + 1] - (real)(2.0) * A[i][j][k] + A[i][j][k - 1]) + A[i][j][k];
+                                       }
+                                   
+                               
+
+#pragma omp for collapse(3) schedule(static)
+                               for (idx_t i = 1; i < n - 1; i++) 
+                                   for (idx_t j = 1; j < n - 1; j++) 
+                                       for (idx_t k = 1; k < n - 1; k++) {
+                                           A[i][j][k] = (real)(0.125) * (B[i + 1][j][k] - (real)(2.0) * B[i][j][k] + B[i - 1][j][k]) + (real)(0.125) * (B[i][j + 1][k] - (real)(2.0) * B[i][j][k] + B[i][j - 1][k]) + (real)(0.125) * (B[i][j][k + 1] - (real)(2.0) * B[i][j][k] + B[i][j][k - 1]) + B[i][j][k];
+                                       }
+                                   
+                               
+                           }
+                       }
+}
+
+
+void run(State &state, pbsize_t pbsize) {
+    pbsize_t tsteps = 1; // 500
+    pbsize_t n = pbsize; // 120
+
+
+
+  auto A = state.allocate_array<real>({n, n, n}, /*fakedata*/ true, /*verify*/ false);
+  auto B = state.allocate_array<real>({n, n, n}, /*fakedata*/ true, /*verify*/ true);
+
+
+  for (auto &&_ : state)
+    kernel(tsteps, n, A, B);
+}
