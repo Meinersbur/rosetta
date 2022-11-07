d3434e53559450dd5381a07accd700d03ce76a48
Michael Kruse <xcellent-test-suite@meinersbur.de>
2022-11-06 22:56:37 -0600

    Add omp_parallel ludcmp

---
+++ benchmarks/suites/polybench/ludcmp/ludcmp-common.cxx
@@ -0,0 +1,14 @@
+#include "ludcmp-common.h"
+
+void ensure_fullrank(pbsize_t n,  multarray<real, 2> A) {
+    real maximum=0;
+    for (idx_t i = 0; i < n ; i++)
+        for (idx_t j = 0; j < n; j++) {
+            auto val = std::abs(A[i][j]);
+            if (val > maximum) maximum = val;
+        }
+
+    // Make the diagnonal elements too large to be a linear combination of the other columns without also making the other vector elements too large.
+    for (idx_t i = 0; i < n ; i++)
+        A[i][i] = std::abs( A[i][i]) + 1 + maximum;
+}
---
+++ benchmarks/suites/polybench/ludcmp/ludcmp-common.h
@@ -0,0 +1,12 @@
+#ifndef LUDCMP_COMMON_H
+#define  LUDCMP_COMMON_H 1
+
+#include <rosetta.h>
+
+void ensure_fullrank(pbsize_t n,  multarray<real, 2> A);
+
+
+
+
+
+#endif /* LUDCMP_COMMON_H */
