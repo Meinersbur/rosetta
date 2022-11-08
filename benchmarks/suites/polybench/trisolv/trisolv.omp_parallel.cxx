7cabbfc9cef53d9a154b3ad185a6bbd8a95103ce
Michael Kruse <xcellent-test-suite@meinersbur.de>
2022-11-08 00:53:07 -0600

    Add omp_parallel trisolv

---
+++ benchmarks/suites/polybench/trisolv/trisolv.omp_parallel.cxx
@@ -0,0 +1,28 @@
+// BUILD: add_benchmark(ppm=omp_parallel)
+
+#include "rosetta.h"
+
+
+static void kernel(pbsize_t  n,
+                   multarray<real, 2> L, real x[], real b[]) {
+  for (idx_t i = 0; i < n; i++) {
+    real sum = 0 ;
+#pragma omp parallel for schedule(static) default(none) firstprivate(i,x,L) reduction(+:sum)
+    for (idx_t j = 0; j < i; j++)
+      sum += L[i][j] * x[j];
+    x[i] =  (b[i] - sum) / L[i][i];
+  }
+}
+
+
+void run(State &state, pbsize_t pbsize) {
+    pbsize_t n = pbsize; // 2000
+
+
+  auto L = state.allocate_array<real>({n, n}, /*fakedata*/ true, /*verify*/ false, "L");
+  auto x = state.allocate_array<real>({n}, /*fakedata*/ true, /*verify*/ true,"x");
+  auto b = state.allocate_array<real>({n}, /*fakedata*/ true, /*verify*/ false,"b");
+
+  for (auto &&_ : state)
+    kernel(n, L, x, b);
+}
