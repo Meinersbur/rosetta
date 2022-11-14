54a9c1649de82fdaab6edb825a99802a45fec547
Michael Kruse <jlse@meinersbur.de>
2022-11-14 05:22:29 +0000

    Add CUDA adi

--- benchmarks/suites/polybench/adi/adi.cxx
+++ benchmarks/suites/polybench/adi/adi.cxx
@@ -4,8 +4,8 @@
 
 
 static void kernel(
-    int tsteps,
-    int n,
+    pbsize_t tsteps,
+    pbsize_t n,
     multarray<real, 2> u,
     multarray<real, 2> v,
     multarray<real, 2> p,
@@ -26,32 +26,32 @@ static void kernel(
   real e = 1 + mul2;
   real f = d;
 
-  for (int t = 1; t <= tsteps; t++) {
+  for (idx_t t = 1; t <= tsteps; t++) {
     // Column Sweep
-    for (int i = 1; i < n - 1; i++) {
+    for (idx_t i = 1; i < n - 1; i++) {
       v[0][i] = 1;
       p[i][0] = 0;
       q[i][0] = v[0][i];
-      for (int j = 1; j < n - 1; j++) {
+      for (idx_t j = 1; j < n - 1; j++) {
         p[i][j] = -c / (a * p[i][j - 1] + b);
         q[i][j] = (-d * u[j][i - 1] + (1 + 2 * d) * u[j][i] - f * u[j][i + 1] - a * q[i][j - 1]) / (a * p[i][j - 1] + b);
       }
 
       v[n - 1][i] = 1;
-      for (int j = n - 2; j >= 1; j--)
+      for (idx_t j = n - 2; j >= 1; j--)
         v[j][i] = p[i][j] * v[j + 1][i] + q[i][j];
     }
     // Row Sweep
-    for (int i = 1; i < n - 1; i++) {
+    for (idx_t i = 1; i < n - 1; i++) {
       u[i][0] = 1;
       p[i][0] = 0;
       q[i][0] = u[i][0];
-      for (int j = 1; j < n - 1; j++) {
+      for (idx_t j = 1; j < n - 1; j++) {
         p[i][j] = -f / (d * p[i][j - 1] + e);
         q[i][j] = (-a * v[i - 1][j] + (1 + 2 * a) * v[i][j] - c * v[i + 1][j] - d * q[i][j - 1]) / (d * p[i][j - 1] + e);
       }
       u[i][n - 1] = 1;
-      for (int j = n - 2; j >= 1; j--)
+      for (idx_t j = n - 2; j >= 1; j--)
         u[i][j] = p[i][j] * u[i][j + 1] + q[i][j];
     }
   }
@@ -60,16 +60,16 @@ static void kernel(
 
 
 
-void run(State &state, int pbsize) {
-  size_t tsteps = pbsize / 2; // 500
-  size_t n = pbsize;          // 1000
+void run(State &state, pbsize_t pbsize) {
+  pbsize_t tsteps = pbsize / 2; // 500
+  pbsize_t n = pbsize;          // 1000
 
 
 
-  auto u = state.allocate_array<double>({n, n}, /*fakedata*/ true, /*verify*/ true);
-  auto v = state.allocate_array<double>({n, n}, /*fakedata*/ false, /*verify*/ false);
-  auto p = state.allocate_array<double>({n, n}, /*fakedata*/ false, /*verify*/ false);
-  auto q = state.allocate_array<double>({n, n}, /*fakedata*/ false, /*verify*/ false);
+ auto u = state.allocate_array<double>({n, n}, /*fakedata*/ true, /*verify*/ true,"u");
+  auto v = state.allocate_array<double>({n, n}, /*fakedata*/ false, /*verify*/ false,"v");
+  auto p = state.allocate_array<double>({n, n}, /*fakedata*/ false, /*verify*/ false, "p");
+  auto q = state.allocate_array<double>({n, n}, /*fakedata*/ false, /*verify*/ false, "q");
 
 
 
