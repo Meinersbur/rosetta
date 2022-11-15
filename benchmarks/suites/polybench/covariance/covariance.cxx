10dc546284060d0c04c1cbc6ebf83029da00664a
Michael Kruse <jlse@meinersbur.de>
2022-11-15 03:03:23 +0000

    Add CUDA covariance

--- benchmarks/suites/polybench/covariance/covariance.cxx
+++ benchmarks/suites/polybench/covariance/covariance.cxx
@@ -3,26 +3,26 @@
 #include "rosetta.h"
 
 
-static void kernel(int m, int n,
+static void kernel(pbsize_t m, pbsize_t n,
                    multarray<real, 2> data,
                    multarray<real, 2> cov,
-                   real *mean) {
+                   real mean[]) {
 #pragma scop
-  for (int j = 0; j < m; j++) {
+  for (idx_t j = 0; j < m; j++) {
     mean[j] = 0.0;
-    for (int i = 0; i < n; i++)
+    for (idx_t i = 0; i < n; i++)
       mean[j] += data[i][j];
     mean[j] /= n;
   }
 
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < m; j++)
+  for (idx_t i = 0; i < n; i++)
+    for (idx_t j = 0; j < m; j++)
       data[i][j] -= mean[j];
 
-  for (int i = 0; i < m; i++)
-    for (int j = i; j < m; j++) {
+  for (idx_t i = 0; i < m; i++)
+    for (idx_t j = i; j < m; j++) {
       cov[i][j] = 0.0;
-      for (int k = 0; k < n; k++)
+      for (idx_t k = 0; k < n; k++)
         cov[i][j] += data[k][i] * data[k][j];
       cov[i][j] /= (n - 1.0);
       cov[j][i] = cov[i][j];
@@ -31,14 +31,15 @@ static void kernel(int m, int n,
 }
 
 
-void run(State &state, int pbsize) {
+void run(State &state, pbsize_t pbsize) {
   // n is 8%-25% larger than m
-  size_t n = pbsize;
-  size_t m = pbsize - pbsize / 8;
+  pbsize_t n = pbsize;
+  pbsize_t m = pbsize - pbsize / 8;
 
-  auto data = state.allocate_array<real>({ n, m}, /*fakedata*/ true, /*verify*/ false);
-  auto cov = state.allocate_array<real>({m, m}, /*fakedata*/ false, /*verify*/ true);
-  auto mean = state.allocate_array<real>({m}, /*fakedata*/ false, /*verify*/ true);
+  auto data = state.allocate_array<real>({ n, m}, /*fakedata*/ true, /*verify*/ false, "data");
+    auto mean = state.allocate_array<real>({m}, /*fakedata*/ false, /*verify*/ true, "mean");
+  auto cov = state.allocate_array<real>({m, m}, /*fakedata*/ false, /*verify*/ true, "cov");
+
 
 
   for (auto &&_ : state)
