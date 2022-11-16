710788f9e6347a4222dba05527d329a9d69023df
Michael Kruse <jlse@meinersbur.de>
2022-11-16 02:47:53 +0000

    Add CUDA fdtf-2d

--- benchmarks/suites/polybench/fdtd-2d/fdtd-2d.omp_parallel.cxx
+++ benchmarks/suites/polybench/fdtd-2d/fdtd-2d.omp_parallel.cxx
@@ -4,32 +4,31 @@
 
 
 
-static void kernel(int tmax,
-                   int nx,
-                   int ny,
+static void kernel(pbsize_t  tmax,
+                   pbsize_t nx,
+                   pbsize_t ny,
                    multarray<real, 2> ex, multarray<real, 2> ey, multarray<real, 2> hz, real fict[]) {
 #pragma omp parallel default(none) firstprivate(tmax,nx,ny,ex,ey,hz,fict)
                        {
 
-                           for (int t = 0; t < tmax; t++) {
-
+                           for (idx_t t = 0; t < tmax; t++) {
 #pragma omp for schedule(static)
-                               for (int j = 0; j < ny; j++)
+                               for (idx_t j = 0; j < ny; j++)
                                    ey[0][j] = fict[t];
 
 #pragma omp for collapse(2) schedule(static) nowait
-                               for (int i = 1; i < nx; i++)
-                                   for (int j = 0; j < ny; j++)
+                               for (idx_t i = 1; i < nx; i++)
+                                   for (idx_t j = 0; j < ny; j++)
                                        ey[i][j] -= (real)(0.5) * (hz[i][j] - hz[i - 1][j]);
 
 #pragma omp for collapse(2) schedule(static)
-                               for (int i = 0; i < nx; i++)
-                                   for (int j = 1; j < ny; j++)
+                               for (idx_t i = 0; i < nx; i++)
+                                   for (idx_t j = 1; j < ny; j++)
                                        ex[i][j] -=  (real)(0.5) * (hz[i][j] - hz[i][j - 1]);
 
 #pragma omp for collapse(2) schedule(static)
-                               for (int i = 0; i < nx - 1; i++)
-                                   for (int j = 0; j < ny - 1; j++)
+                               for (idx_t i = 0; i < nx - 1; i++)
+                                   for (idx_t j = 0; j < ny - 1; j++)
                                        hz[i][j] -=  (real)(0.7) * (ex[i][j + 1] - ex[i][j] +  ey[i + 1][j] - ey[i][j]);
                                
                            }
@@ -37,17 +36,17 @@ static void kernel(int tmax,
 }
 
 
-void run(State &state, int pbsize) {
-  size_t tmax = 5 * pbsize / 12;   // 500
-  size_t nx = pbsize - pbsize / 6; // 1000
-  size_t ny = pbsize;              // 1200
+void run(State &state, pbsize_t pbsize) {
+  pbsize_t tmax = 5 * pbsize / 12;   // 500
+  pbsize_t nx = pbsize - pbsize / 6; // 1000
+  pbsize_t ny = pbsize;              // 1200
 
 
 
-  auto ex = state.allocate_array<real>({nx, ny}, /*fakedata*/ true, /*verify*/ true);
-  auto ey = state.allocate_array<real>({nx, ny}, /*fakedata*/ true, /*verify*/ true);
-  auto hz = state.allocate_array<real>({nx, ny}, /*fakedata*/ true, /*verify*/ true);
-  auto fict = state.allocate_array<real>({tmax}, /*fakedata*/ true, /*verify*/ false);
+  auto ex = state.allocate_array<real>({nx, ny}, /*fakedata*/ true, /*verify*/ true, "ex");
+  auto ey = state.allocate_array<real>({nx, ny}, /*fakedata*/ true, /*verify*/ true, "ey");
+  auto hz = state.allocate_array<real>({nx, ny}, /*fakedata*/ true, /*verify*/ true, "hz");
+  auto fict = state.allocate_array<real>({tmax}, /*fakedata*/ true, /*verify*/ false, "fict");
 
 
 
