4decd75889138cad899f6daac445fbdec7019585
Michael Kruse <xcellent-test-suite@meinersbur.de>
2022-11-18 17:00:27 -0600

    update-format also invokes autopip8

--- rosetta/src/rosetta/scripts/benchmark.py
+++ rosetta/src/rosetta/scripts/benchmark.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from rosetta.util import support
+import rosetta
+from rosetta import *
 import sys
 from collections import defaultdict
 import sys
@@ -12,34 +15,29 @@ import re
 import importlib
 
 
-
-
-
 script = Path(sys.argv[0]).absolute()
 thisscript = Path(__file__)
 thisscriptdir = thisscript.parent
 
 #sys.path.insert(0,str( (thisscript.parent / 'rosetta' /  'lib').absolute() ))
-from rosetta import *
 
-import rosetta
 runner = rosetta.runner
-from rosetta.util import support
+
 
 class BuildConfig:
-    def __init__(self,name,ppm,cmake_arg,cmake_def,compiler_arg,compiler_def):
+    def __init__(self, name, ppm, cmake_arg, cmake_def, compiler_arg, compiler_def):
         self.name = name
         self.ppm = set(ppm)
         self.cmake_arg = cmake_arg
         self.cmake_def = cmake_def
-        self.compiler_arg = compiler_arg 
+        self.compiler_arg = compiler_arg
         self.compiler_def = compiler_def
 
         # TODO: select compiler executable
 
     def gen_cmake_args(self):
         compiler_args = self.compiler_arg.copy()
-        for k,v in self.compiler_def:
+        for k, v in self.compiler_def:
             if v:
                 compiler_args.append(f"-D{k}")
             else:
@@ -47,22 +45,23 @@ class BuildConfig:
 
         # TODO: Combine with (-D, -DCMAKE_<lang>_FLAGS) from compiler/cmake_arg
         cmake_opts = self.cmake_arg[:]
-        for k,d in self.cmake_def.items():
+        for k, d in self.cmake_def.items():
             cmake_opts .append(f"-D{k}={d}")
         if compiler_args:
             # TODO: Only set the ones relevant for enable PPMs
-            opt_args  = shjoin(compiler_args)
-            cmake_opts += [f"-DCMAKE_C_FLAGS={opt_args}", f"-DCMAKE_CXX_FLAGS={opt_args}", f"-DCMAKE_CUDA_FLAGS={opt_args}"] # TODO: Release flags?
+            opt_args = shjoin(compiler_args)
+            cmake_opts += [f"-DCMAKE_C_FLAGS={opt_args}", f"-DCMAKE_CXX_FLAGS={opt_args}",
+                           f"-DCMAKE_CUDA_FLAGS={opt_args}"]  # TODO: Release flags?
 
         if self.ppm:
-          # TODO: Case, shortcuts
-          for ppm in ['serial', 'cuda', 'openmp-thread', 'openmp-task', 'openmp-target']:
-            ucase_name = ppm.upper().replace('-','_')
-            if ppm in self.ppm:
-                cmake_opts.append(f"-DROSETTA_PPM_{ucase_name}=ON")
-            else:
-                # TODO: switch to have default OFF, so we don't need to list all of them
-                cmake_opts.append(f"-DROSETTA_PPM_{ucase_name}=OFF")
+            # TODO: Case, shortcuts
+            for ppm in ['serial', 'cuda', 'openmp-thread', 'openmp-task', 'openmp-target']:
+                ucase_name = ppm.upper().replace('-', '_')
+                if ppm in self.ppm:
+                    cmake_opts.append(f"-DROSETTA_PPM_{ucase_name}=ON")
+                else:
+                    # TODO: switch to have default OFF, so we don't need to list all of them
+                    cmake_opts.append(f"-DROSETTA_PPM_{ucase_name}=OFF")
 
         if self.name:
             cmake_opts.append(f"-DROSETTA_CONFIGNAME={self.name}")
@@ -70,15 +69,17 @@ class BuildConfig:
         return cmake_opts
 
 
-def make_buildconfig(name,ppm,cmake_arg,cmake_def,compiler_arg,compiler_def):
-    return BuildConfig(name,ppm,cmake_arg,cmake_def,compiler_arg,compiler_def)
+def make_buildconfig(name, ppm, cmake_arg, cmake_def, compiler_arg, compiler_def):
+    return BuildConfig(name, ppm, cmake_arg, cmake_def, compiler_arg, compiler_def)
 
 
-configsplitarg =         re.compile(r'((?P<configname>[a-zA-Z0-9_]+)\:)?(?P<arg>.*)')
-configsplitdef =         re.compile(r'((?P<configname>[a-zA-Z0-9_]+)\:)?(?P<defname>[a-zA-Z0-9_]+)(\=(?P<defvalue>.*))?')
+configsplitarg = re.compile(r'((?P<configname>[a-zA-Z0-9_]+)\:)?(?P<arg>.*)')
+configsplitdef = re.compile(r'((?P<configname>[a-zA-Z0-9_]+)\:)?(?P<defname>[a-zA-Z0-9_]+)(\=(?P<defvalue>.*))?')
 configsplitdefrequired = re.compile(r'((?P<configname>[a-zA-Z0-9_]+)\:)?(?P<defname>[a-zA-Z0-9_]+)\=(?P<defvalue>.*)')
-configppm =              re.compile(r'((?P<configname>[a-zA-Z0-9_]+)\:)?(?P<ppm>[a-zA-Z\-]+)')
-def parse_build_configs(args,implicit_reference):
+configppm = re.compile(r'((?P<configname>[a-zA-Z0-9_]+)\:)?(?P<ppm>[a-zA-Z\-]+)')
+
+
+def parse_build_configs(args, implicit_reference):
     def parse_arglists(l):
         raw = defaultdict(lambda: [])
         if l is None:
@@ -89,7 +90,8 @@ def parse_build_configs(args,implicit_reference):
             value = m.group('arg')
             raw[configname].append(value)
         return raw
-    def parse_deflists(l,valrequired):
+
+    def parse_deflists(l, valrequired):
         raw = defaultdict(lambda: dict())
         if l is None:
             return raw
@@ -97,14 +99,14 @@ def parse_build_configs(args,implicit_reference):
             m = (configsplitdefrequired if valrequired else configsplitdefrequired).fullmatch(arg)
             configname = m.group('configname') or ''
             defname = m.group('defname')
-            defvalue = m.group('defvalue') # Can be none
+            defvalue = m.group('defvalue')  # Can be none
             raw[configname][defname] = defvalue
         return raw
 
     cmake_arg = parse_arglists(args.cmake_arg)
-    cmake_def = parse_deflists(args.cmake_def,valrequired=True)
+    cmake_def = parse_deflists(args.cmake_def, valrequired=True)
     compiler_arg = parse_arglists(args.compiler_arg)
-    compiler_def = parse_deflists(args.compiler_def,valrequired=False)
+    compiler_def = parse_deflists(args.compiler_def, valrequired=False)
     ppm = parse_arglists(args.ppm)
 
     keys = OrderedSet()
@@ -121,25 +123,28 @@ def parse_build_configs(args,implicit_reference):
         if not k:
             continue
         # TODO: Handle duplicate defs (specific override general)
-        configs.append(BuildConfig(k, ppm[''] +  ppm[k],  cmake_arg=cmake_arg[''] + cmake_arg[k] ,  cmake_def=cmake_def[''] | cmake_def[k],  compiler_arg=compiler_arg[''] + compiler_arg[k], compiler_def=compiler_def[''] | compiler_def[k] ))
+        configs.append(BuildConfig(k, ppm[''] + ppm[k],  cmake_arg=cmake_arg[''] + cmake_arg[k],  cmake_def=cmake_def[''] |
+                       cmake_def[k],  compiler_arg=compiler_arg[''] + compiler_arg[k], compiler_def=compiler_def[''] | compiler_def[k]))
     # Use single config if not "CONFIG:" is specified
     if not configs:
-       configs.append(BuildConfig(None, ppm[''], cmake_arg=cmake_arg[''] ,  cmake_def=cmake_def[''],  compiler_arg=compiler_arg[''] , compiler_def=compiler_def[''] ) )
+        configs.append(BuildConfig(None, ppm[''], cmake_arg=cmake_arg[''],  cmake_def=cmake_def[''],
+                       compiler_arg=compiler_arg[''], compiler_def=compiler_def['']))
     return configs
+
 
 def print_verbose(*args, **kwargs):
     if verbose:
         print(*args, **kwargs)
 
+
 def invoke_verbose(*args, **kwargs):
-    if verbose :
+    if verbose:
         invoke.diag(*args, **kwargs)
     else:
         invoke.run(*args, **kwargs)
 
 
-
-def main(argv,rootdir=None):
+def main(argv, rootdir=None):
     global verbose
     parser = argparse.ArgumentParser(description="Benchmark configure, build, execute & evaluate", allow_abbrev=False)
 
@@ -152,7 +157,7 @@ def main(argv,rootdir=None):
 
     # Configure step
     add_boolean_argument(parser, 'configure', default=True, help="Enable configure (CMake) step")
-    #TODO: Add switches that parse multiple arguments using shsplit
+    # TODO: Add switches that parse multiple arguments using shsplit
     parser.add_argument('--config', metavar="CONFIG", action='append')
     parser.add_argument('--ppm', metavar="CONFIG:PPM", action='append')
     parser.add_argument('--cmake-arg', metavar="CONFIG:ARG", action='append')
@@ -160,27 +165,25 @@ def main(argv,rootdir=None):
     parser.add_argument('--compiler-arg', metavar="CONFIG:ARG", action='append')
     parser.add_argument('--compiler-def', metavar="CONFIG:DEF[=VAL]", action='append')
 
-
     # Build step
     add_boolean_argument(parser, 'build', default=True, help="Enable build step")
 
     # Run/verify/evaluate steps
-    runner.subcommand_run(parser,None,srcdir=thisscriptdir)
-
+    runner.subcommand_run(parser, None, srcdir=thisscriptdir)
 
     args = parser.parse_args(argv[1:])
     verbose = args.verbose
 
     with runner.globalctxmgr:
 
-        # TODO: If not specified, just reuse existing configs 
-        configs = parse_build_configs(args,implicit_reference=args.verify)
+        # TODO: If not specified, just reuse existing configs
+        configs = parse_build_configs(args, implicit_reference=args.verify)
 
-        srcdir = first_defined(args.rootdir,rootdir,pathlib.Path.cwd())
+        srcdir = first_defined(args.rootdir, rootdir, pathlib.Path.cwd())
         builddir = srcdir / 'build'
         resultdir = builddir / 'results'
 
-        #if builddir.exists():
+        # if builddir.exists():
         #    if args.clean:
         #        # TODO: Do this automatically when necessary (hash the CMakeLists.txt)
         #        print_verbose("Cleaning previous builds")
@@ -190,29 +193,27 @@ def main(argv,rootdir=None):
         #            shutil.rmtree(c)
         #    else:
         #        print_verbose("Reusing existing build")
-        resultdir.mkdir(parents=True,exist_ok=True)
+        resultdir.mkdir(parents=True, exist_ok=True)
 
-        
         for config in configs:
             if not config.name:
-                config.builddir  = builddir / 'defaultbuild'
+                config.builddir = builddir / 'defaultbuild'
             elif config.name == "REF":
                 # TODO: same as defaultbuild?
-                config.builddir  = builddir / 'refbuild'
+                config.builddir = builddir / 'refbuild'
             else:
                 config.builddir = builddir / f'build-{config.name}'
-
 
         # TODO: Recognize "module" system
         # TODO: Recognize famous environments (JLSE, Theta, Summit, Frontier, Aurora, ...)
 
-
         for config in configs:
             builddir = config.builddir
-            configdescfile  = builddir/'RosettaCache.txt'
+            configdescfile = builddir/'RosettaCache.txt'
 
             # TODO: Support other generators as well
-            opts = ['cmake', srcdir, '-GNinja Multi-Config', '-DCMAKE_CROSS_CONFIGS=all', f'-DROSETTA_RESULTS_DIR={resultdir}']
+            opts = ['cmake', srcdir, '-GNinja Multi-Config',
+                    '-DCMAKE_CROSS_CONFIGS=all', f'-DROSETTA_RESULTS_DIR={resultdir}']
             opts += config.gen_cmake_args()
             expectedopts = shjoin(opts).rstrip()
 
@@ -220,57 +221,45 @@ def main(argv,rootdir=None):
             if not args.clean and configdescfile.is_file() and (builddir / 'build.ninja').exists():
                 existingopts = readfile(configdescfile).rstrip()
                 if existingopts == expectedopts:
-                    reusebuilddir=True
+                    reusebuilddir = True
 
             if not reusebuilddir:
                 if builddir.exists():
                     shutil.rmtree(builddir)
-                builddir.mkdir(exist_ok=True,parents=True)
+                builddir.mkdir(exist_ok=True, parents=True)
                 invoke_verbose(*opts, cwd=config.builddir)
                 createfile(configdescfile, expectedopts)
 
-
-
-
-
         for config in configs:
             if args.build:
-                # TODO: Select subset to be build 
+                # TODO: Select subset to be build
                 invoke_verbose('ninja', cwd=config.builddir)
-
-
-
-
 
         # Load all available benchmarks
         if args.verify or args.bench or args.probe or (not args.verify and args.bench is None and not args.probe):
             for config in configs:
                 load_register_file(config.builddir / 'benchmarks' / 'benchlist.py')
 
-
-
-
         def only_REF(bench):
-            return bench.configname == 'REF' 
-        def no_ref(bench):
-            return bench.configname != 'REF' 
+            return bench.configname == 'REF'
 
+        def no_ref(bench):
+            return bench.configname != 'REF'
 
         try:
             [refconfig] = (c for c in configs if c.name == 'REF')
         except:
             refconfig = None
-        runner.subcommand_run(None,args,
-            srcdir=thisscriptdir,
-            buildondemand=not args.build,
-            builddirs=[config.builddir for config in configs],
-            refbuilddir= refconfig.builddir if refconfig else None,
-            filterfunc=no_ref,
-            resultdir =resultdir
-            )
+        runner.subcommand_run(None, args,
+                              srcdir=thisscriptdir,
+                              buildondemand=not args.build,
+                              builddirs=[config.builddir for config in configs],
+                              refbuilddir=refconfig.builddir if refconfig else None,
+                              filterfunc=no_ref,
+                              resultdir=resultdir
+                              )
 
-
-        #if args.verify:
+        # if args.verify:
         #   def only_REF(bench):
         #       return bench.configname =='REF'
         #   [refconfig] = (c for c in configs if c.name == 'REF')
@@ -279,15 +268,13 @@ def main(argv,rootdir=None):
         #   runner.ensure_reffiles(problemsizefile=args.problemsizefile,filterfunc=only_REF,srcdir=srcdir,refdir=refdir)
         #   runner.run_verify(problemsizefile=args.problemsizefile,filterfunc=only_REF,srcdir=srcdir,refdir=refdir)
 
-
         #resultfiles = None
-        #if args.run:
+        # if args.run:
         #    # TODO: Filter way 'REF' config
         #    resultfiles = runner.run_bench(srcdir=thisscriptdir, problemsizefile=args.problemsizefile)
         #    #invoke_verbose('cmake', '--build', '.',  '--config','Release', '--target','run', cwd=config.builddir)
-        
 
-        #if args.evaluate:
+        # if args.evaluate:
         #   if not resultfiles:
         #        assert False, "TODO: Lookup last (successful) results dir"
         #    if len(configs) == 1:
@@ -295,14 +282,12 @@ def main(argv,rootdir=None):
         #    else:
         #        runner.results_compare(resultfiles, compare_by="configname", compare_val=["walltime"])
 
-        #if args.boxplot:
+        # if args.boxplot:
         #    def no_ref(bench):
-        #       return bench.configname !='REF' 
+        #       return bench.configname !='REF'
         #    fig = runner.results_boxplot(resultfiles, compare_by="configname", filterfunc=no_ref)
         #    fig.savefig(fname=args.boxplot)
-        #    fig.canvas.draw_idle() 
-
-
+        #    fig.canvas.draw_idle()
 
 
 if __name__ == '__main__':
--- rosetta/src/rosetta/util/support.py
+++ rosetta/src/rosetta/util/support.py
@@ -15,81 +15,66 @@ import stat
 from pathlib import Path
 
 
-
-
-
 shsplit = shlex.split
 
-def shquote(arg,windows=None):
+
+def shquote(arg, windows=None):
     if windows is None:
-        windows = (platform.system()=='Windows')
+        windows = (platform.system() == 'Windows')
     if windows:
         arg = str(arg)
-        arg = arg.replace(r'"',r'""')
+        arg = arg.replace(r'"', r'""')
         if ' ' in arg or '^' in arg:
             return r'"' + arg + r'"'
         return arg
     return shlex.quote(str(arg))
 
 
-def shjoin(args,sep=' ',windows=None):
-  return sep.join([shquote(arg,windows=windows) for arg in args])
+def shjoin(args, sep=' ', windows=None):
+    return sep.join([shquote(arg, windows=windows) for arg in args])
 
 
+def shcombine(arg=None, args=None):
+    result = []
 
-def shcombine(arg=None,args=None):
-  result = []
+    # pre-escaped arguments
+    if arg is not None:
+        result += arg
 
-  # pre-escaped arguments
-  if arg is not None:
-    result += arg
+    # args needing de-escaping
+    if args is not None:
+        result += [arg for arg in shsplit(argline) for argline in args]
 
-  # args needing de-escaping
-  if args is not None:
-    result += [arg for arg in shsplit(argline) for argline in args]
-
-  return result
-
+    return result
 
 
 # from https://stackoverflow.com/questions/1714027/version-number-comparison-in-python
 def version_cmp(v1, v2):
     def convert_int(v):
-      result = []
-      for p in v.split('.'):
-        try: 
-          i = int(p)
-        except:
-          # development versions have their sha1 appended
-          break;
-        result.append(i)
-      return result
+        result = []
+        for p in v.split('.'):
+            try:
+                i = int(p)
+            except:
+                # development versions have their sha1 appended
+                break
+            result.append(i)
+        return result
     parts1 = convert_int(v1)
     parts2 = convert_int(v2)
     parts1, parts2 = zip(*itertools.zip_longest(parts1, parts2, fillvalue=0))
-    return parts1>=parts2
-
-
-
-
-
-
-
-
-
+    return parts1 >= parts2
 
 
 # For objects representing a special value
 class NamedSentinel:
-    def __init__(self,name,**kwargs):
+    def __init__(self, name, **kwargs):
         self.name = name
-        for k,v in kwargs.items():
-          setattr(self,k,v)
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
     def __repr__(self):
         return self.name
-
-
-
 
 
 def mark_executable(path):
@@ -103,8 +88,7 @@ def remove_readonly(func, path, _):
     func(path)
 
 
-
-if os.name == 'nt' and sys.version_info < (3,8):
+if os.name == 'nt' and sys.version_info < (3, 8):
     class TemporaryDirectory(object):
         """Also delete read-only files on Windows
         See https://bugs.python.org/issue26660
@@ -113,7 +97,7 @@ if os.name == 'nt' and sys.version_info < (3,8):
         def __init__(self, suffix="", prefix=tempfile.template, dir=None):
             self.name = tempfile.mkdtemp(suffix, prefix, dir)
             self._finalizer = weakref.finalize(self, self._cleanup, self.name,
-                warn_message="Implicitly cleaning up {!r}".format(self))
+                                               warn_message="Implicitly cleaning up {!r}".format(self))
 
         @classmethod
         def _cleanup(cls, name, warn_message):
@@ -131,75 +115,70 @@ if os.name == 'nt' and sys.version_info < (3,8):
 
         def cleanup(self):
             if self._finalizer.detach():
-                shutil.rmtree(self.name,onerror=remove_readonly)
+                shutil.rmtree(self.name, onerror=remove_readonly)
 else:
     TemporaryDirectory = tempfile.TemporaryDirectory
 
 
-
-def replace_file(src,dst):
+def replace_file(src, dst):
     try:
-        os.replace(src,dst)
+        os.replace(src, dst)
     except:
         os.remove(dst)
-        shutil.move(src,dst)
-
-
-
-
+        shutil.move(src, dst)
 
 
 def ntpath_to_mingwpath(path):
     if os.path is not ntpath:
         return path
-    if isinstance(path,pathlib.PureWindowsPath):
-      path = str(path)
+    if isinstance(path, pathlib.PureWindowsPath):
+        path = str(path)
 
     if len(path) >= 2 and path[0].isalpha() and path[1] == ':':
-      return '/' + path[0].lower() + path[2:].replace('\\', '/')
+        return '/' + path[0].lower() + path[2:].replace('\\', '/')
 
     return path
 
     assert ntpath.isabs(path)
-    drive,path = ntpath.splitdrive(path)
+    drive, path = ntpath.splitdrive(path)
     parts = path.split('\\')[1:]
 
     if drive:
-        parts =  ['/' + drive[:-1].lower()] + parts
+        parts = ['/' + drive[:-1].lower()] + parts
 
     result = posixpath.join(*parts)
     return result
 
 
-
 def mingwpath_to_ntpath(path):
-  if os.path is not ntpath:
-    if isinstance(path,Path):
-      return path
-    return Path(path)
+    if os.path is not ntpath:
+        if isinstance(path, Path):
+            return path
+        return Path(path)
 
-  # Detect absolute MinGW paths
-  if len(path) == 1 and path == '/':
-    return Path('\\')
-  if len(path) ==2 and path[0] =='/' and path[1].isalpha():
-    return Path(path[1] + ':')
-  if len(path) >= 3 and path[0]=='/' and path[1].isalpha() and path[2]=='/':
-    return Path(path[1] + ':\\' +  path[3:].replace('/','\\'))
+    # Detect absolute MinGW paths
+    if len(path) == 1 and path == '/':
+        return Path('\\')
+    if len(path) == 2 and path[0] == '/' and path[1].isalpha():
+        return Path(path[1] + ':')
+    if len(path) >= 3 and path[0] == '/' and path[1].isalpha() and path[2] == '/':
+        return Path(path[1] + ':\\' + path[3:].replace('/', '\\'))
 
-  return mkpath(path)
+    return mkpath(path)
 
 
 def mkpath(path):
-  if path == None:
-    return None
-  if isinstance(path,pathlib.Path):
-    return path
-  return pathlib.Path(path)
+    if path == None:
+        return None
+    if isinstance(path, pathlib.Path):
+        return path
+    return pathlib.Path(path)
+
 
 def mkpurepath(path):
-  if isinstance(path,pathlib.PurePath):
-    return path
-  return pathlib.PurePath(path)
+    if isinstance(path, pathlib.PurePath):
+        return path
+    return pathlib.PurePath(path)
 
 
 def first_existing(*args):
@@ -211,7 +190,9 @@ def first_existing(*args):
 
 # Like (.. or .. or ..) but only considers 'None' being false-y (and no short-circuit)
 # fallback= not really necessary, just to make it explicit
-def first_defined(*args,fallback=None):
+
+
+def first_defined(*args, fallback=None):
     for arg in args:
         if arg is not None:
             return arg
@@ -226,60 +207,58 @@ def first_defined(*args,fallback=None):
 # nomatch: Default return of no substitution matches; no substitutions are applied
 #
 # return predefined choice or substitution, number of matches of original word (or False if fallback to default)
-def predefined(word,*substitutions,default=None):
-  if word is None:
-    if default is None:
-      return None,False
-    result,_ = predefined(default,*substitutions)
-    return result,False
+def predefined(word, *substitutions, default=None):
+    if word is None:
+        if default is None:
+            return None, False
+        result, _ = predefined(default, *substitutions)
+        return result, False
 
-  rounds = 0
-  for subst in substitutions:
-    word_lower = word.lower()
-    if word_lower in subst:
-      rounds += 1
-      if isinstance(subst,set) or isinstance(subst,list):
-        word = word_lower
-      else:
-        word = subst[word_lower]
-  if not rounds:
-    return word,True
-  return word,rounds
+    rounds = 0
+    for subst in substitutions:
+        word_lower = word.lower()
+        if word_lower in subst:
+            rounds += 1
+            if isinstance(subst, set) or isinstance(subst, list):
+                word = word_lower
+            else:
+                word = subst[word_lower]
+    if not rounds:
+        return word, True
+    return word, rounds
 
 
-
-def predefined_fallback(word,*substitutions,fallback=None):
-  result,rounds = predefined(word,*substitutions)
-  if not rounds or rounds is True:
-      if fallback is None:
-        return None,False
-      result,_ = predefined(fallback,*substitutions)
-      return result, False
-  return result,rounds
+def predefined_fallback(word, *substitutions, fallback=None):
+    result, rounds = predefined(word, *substitutions)
+    if not rounds or rounds is True:
+        if fallback is None:
+            return None, False
+        result, _ = predefined(fallback, *substitutions)
+        return result, False
+    return result, rounds
 
 
 # Like predefined, but throws an exception when no match.
-PREDEFINED_NODEFAULT=NamedSentinel('NODEFAULT')
-def predefined_strict(word,*substitutions,default=PREDEFINED_NODEFAULT):
-  if word is None:
-    if default is None:
-      return None
-    if default is PREDEFINED_NODEFAULT:
-      raise Exception("{word} is not one of the predefined values".format(word=word))
-    result,_ = predefined(default,*substitutions)
+PREDEFINED_NODEFAULT = NamedSentinel('NODEFAULT')
+
+
+def predefined_strict(word, *substitutions, default=PREDEFINED_NODEFAULT):
+    if word is None:
+        if default is None:
+            return None
+        if default is PREDEFINED_NODEFAULT:
+            raise Exception("{word} is not one of the predefined values".format(word=word))
+        result, _ = predefined(default, *substitutions)
+        return result
+
+    result, rounds = predefined(word, *substitutions, default=default)
+    #assert rounds, "word must be one of the predefined values"
+    if not rounds or rounds is True:
+        raise Exception("{word} is not one of the predefined values".format(word=word))
     return result
 
 
-
-  result,rounds = predefined(word,*substitutions,default=default)
-  #assert rounds, "word must be one of the predefined values"
-  if not rounds or rounds is True:
-    raise Exception("{word} is not one of the predefined values".format(word=word))
-  return result
-
-
-
-def last_defined(*args,fallback=None):
+def last_defined(*args, fallback=None):
     for arg in reversed(args):
         if arg is not None:
             return arg
@@ -315,19 +294,17 @@ def max_none(first, *args):
 
 
 def readfile(filepath):
-  filepath = mkpath(filepath)
-  with filepath.open('r') as f:
-    return f.read()
+    filepath = mkpath(filepath)
+    with filepath.open('r') as f:
+        return f.read()
 
-def createfile(filename,contents):
-    if isinstance(filename,Path):
-      with filename.open(mode='w+') as f:
-        print(contents,file=f)
+
+def createfile(filename, contents):
+    if isinstance(filename, Path):
+        with filename.open(mode='w+') as f:
+            print(contents, file=f)
     with open(filename, 'w+')as f:
-        print(contents,file=f)
-
-
-
+        print(contents, file=f)
 
 
 def program_exists(cmd):
@@ -348,44 +325,42 @@ def die(msg):
     sys.exit(1)
 
 
-def unique(list1): 
-    unique_list = [] 
-    for x in list1: 
-        if x not in unique_list: 
-            unique_list.append(x) 
+def unique(list1):
+    unique_list = []
+    for x in list1:
+        if x not in unique_list:
+            unique_list.append(x)
     return unique_list
-      
 
-def join_natural(l, separator=', ', lastseparator = ' and '):
-  l = list(l)
-  n = len(l)
-  result = ''
-  for i,v in enumerate(l):
-    if i == 0:
-      result = str(v)
-    elif i == n-1:
-      result += str(lastseparator) + str(v)
-    else:
-      result +=  str(separator) + str(v)
-  return result
 
+def join_natural(l, separator=', ', lastseparator=' and '):
+    l = list(l)
+    n = len(l)
+    result = ''
+    for i, v in enumerate(l):
+        if i == 0:
+            result = str(v)
+        elif i == n-1:
+            result += str(lastseparator) + str(v)
+        else:
+            result += str(separator) + str(v)
+    return result
 
 
 def pyescape(s):
-  s = str(s)
-  return s.replace('\\', '\\\\').replace('"', '\\\"') 
+    s = str(s)
+    return s.replace('\\', '\\\\').replace('"', '\\\"')
 
 
 def pystr(s):
-  return f'"{pyescape(s)}"'
-
-  
-
-def removesuffix(s:str,suffix:str):
-  if s.endswith(suffix):
-    return s[:-len(suffix)]
-  return s
+    return f'"{pyescape(s)}"'
 
 
-assert removesuffix("mysuffix","suffix") == "my"
-assert removesuffix("mynofix","suffix") == "mynofix"
+def removesuffix(s: str, suffix: str):
+    if s.endswith(suffix):
+        return s[:-len(suffix)]
+    return s
+
+
+assert removesuffix("mysuffix", "suffix") == "my"
+assert removesuffix("mynofix", "suffix") == "mynofix"
