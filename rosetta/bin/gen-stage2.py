441c2ffc47ea8443f9e06a66550366133e9cc548
Michael Kruse <xcellent-test-suite@meinersbur.de>
2023-04-24 14:49:07 -0500

    Undo some update-format

---
+++ rosetta/bin/gen-stage2.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)), "src"))
+
+
+rosetta = __import__("rosetta.scripts.gen-stage2")
+if __name__ == '__main__':
+    retcode = getattr(rosetta.scripts,'gen-stage2').main()
+    if retcode:
+        exit(retcode)
