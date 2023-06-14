9827bd3c8504ef6540c8eb817ba4a37d83ee5a70
Arindam Khanda <arindam.khanda@gmail.com>
2023-06-14 10:36:58 -0500

    Changed accessor mode to write

--- benchmarks/idioms/assign/assign.sycl.cxx
+++ benchmarks/idioms/assign/assign.sycl.cxx
@@ -1,28 +1,25 @@
 // BUILD: add_benchmark(ppm=sycl)
+#include <CL/sycl.hpp>
 #include <rosetta.h>
-#include <CL/sycl.hpp> 
 
 
-static void kernel_test(pbsize_t n, real data[]) {
-  cl::sycl::queue q;
+static void kernel_test(cl::sycl::queue q, pbsize_t n, real data[]) {
   cl::sycl::buffer<real, 1> buf(data, cl::sycl::range<1>(n));
-
-    q.submit([&](cl::sycl::handler& cgh) {
-        auto acc = buf.get_access<cl::sycl::access::mode::read_write>(cgh);
-
-        cgh.parallel_for<class AssignNumbers>(
-            cl::sycl::range<1>(n),
-            [=](cl::sycl::id<1> id) {
-                acc[id] = id;
-            });
-    });
-    q.wait_and_throw();
+  q.submit([&](cl::sycl::handler &cgh) {
+    auto acc = buf.get_access<cl::sycl::access::mode::write>(cgh);
+    cgh.parallel_for<class AssignNumbers>(
+        cl::sycl::range<1>(n),
+        [=](cl::sycl::id<1> id) {
+          acc[id] = id;
+        });
+  });
+  q.wait_and_throw();
 }
 
 void run(State &state, pbsize_t n) {
   auto data = state.allocate_array<real>({n}, /*fakedata*/ false, /*verify*/ true, "data");
 
+  cl::sycl::queue q(cl::sycl::default_selector_v);
   for (auto &&_ : state)
-    kernel_test(n, data);
-  
+    kernel_test(q, n, data);
 }
