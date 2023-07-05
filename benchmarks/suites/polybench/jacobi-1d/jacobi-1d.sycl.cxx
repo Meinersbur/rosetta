17e2cf59c5fef9f207d2b6c301ce0c70363e3390
Arindam Khanda <arindam.khanda@gmail.com>
2023-07-05 16:55:53 -0500

    Modifying to use same function while submitting queue

--- benchmarks/suites/polybench/jacobi-1d/jacobi-1d.sycl.cxx
+++ benchmarks/suites/polybench/jacobi-1d/jacobi-1d.sycl.cxx
@@ -4,25 +4,21 @@
 
 using namespace cl::sycl;
 
+void func(handler &cgh, pbsize_t tsteps, pbsize_t n, buffer<real, 1> &A_buf, buffer<real> &B_buf) {
+  auto A_acc = A_buf.get_access<access::mode::read>(cgh);
+  auto B_acc = B_buf.get_access<access::mode::write>(cgh);
+
+  cgh.parallel_for<class kernel_step1>(range<1>(n - 2), [=](id<1> idx) {
+    idx_t i = idx + 1;
+    B_acc[i] = (A_acc[i - 1] + A_acc[i] + A_acc[i + 1]) / 3;
+  });
+}
+
 static void mykernel(queue &q, pbsize_t tsteps, pbsize_t n, buffer<real, 1> &A_buf, buffer<real> &B_buf) {
   for (pbsize_t t = 0; t < tsteps; t++) {
-    q.submit([&](handler &cgh) {
-      auto A_acc = A_buf.get_access<access::mode::read>(cgh);
-      auto B_acc = B_buf.get_access<access::mode::write>(cgh);
+    q.submit([&](handler &cgh) { func(cgh, tsteps, n, A_buf, B_buf); });
 
-      cgh.parallel_for<class kernel_step1>(range<1>(n - 2), [=](id<1> i) {
-        B_acc[i + 1] = (A_acc[i] + A_acc[i + 1] + A_acc[i + 2]) / 3;
-      });
-    });
-
-    q.submit([&](handler &cgh) {
-      auto A_acc = A_buf.get_access<access::mode::write>(cgh);
-      auto B_acc = B_buf.get_access<access::mode::read>(cgh);
-
-      cgh.parallel_for<class kernel_step2>(range<1>(n - 2), [=](id<1> i) {
-        A_acc[i + 1] = (B_acc[i] + B_acc[i + 1] + B_acc[i + 2]) / 3;
-      });
-    });
+    q.submit([&](handler &cgh) { func(cgh, tsteps, n, B_buf, A_buf); });
   }
   q.wait_and_throw();
 }
